aca62cde0a30883a4da872ca745518a8fb710ba4
Mael Clerambault <mael@clerambault.fr>
2009-08-16 17:57:11 +0200

    Add default path for socket

--- saslauth.rb
+++ saslauth.rb
@@ -2,7 +2,7 @@ require 'socket'
 
 class SASLauth
   Version="0.1"
-  def initialize(path); @path = path end
+  def initialize(path = "/var/run/saslauthd/mux"); @path = path end
   def authenticate(username, password, service = "imap", realm = "")
     a = [username.size, username, password.size, password, service.size, service, realm.size, realm]
     s = a.pack("na*na*na*na*")
