cab93d7fe9efae59be72e45d3010248d04ead8f0
Mael Clerambault <mael@clerambault.fr>
2009-08-16 17:52:58 +0200

    Formatting

--- saslauth.rb
+++ saslauth.rb
@@ -1,20 +1,14 @@
 require 'socket'
 
 class SASLauth
-def initialize(path); @path = path end
-def authenticate(username, password, service = "imap", realm = "")
-  a = [username.size, username,
-       password.size, password,
-       service.size, service,
-       realm.size, realm ]
-  s = a.pack("na*na*na*na*")
-  UNIXSocket.open(@path) {|f| 
-    f.write s
-    if f.read.unpack("na*")[1] =~ /^OK/
-      true
-    else
-      false
-    end
-  }
-end
+  Version="0.1"
+  def initialize(path); @path = path end
+  def authenticate(username, password, service = "imap", realm = "")
+    a = [username.size, username, password.size, password, service.size, service, realm.size, realm]
+    s = a.pack("na*na*na*na*")
+    UNIXSocket.open(@path) {|f| 
+      f.write s
+      f.read.unpack("na*")[1] =~ /^OK/ ? true : false
+    }
+  end
 end
